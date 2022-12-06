77b4a3dbc314c3c6183419b667b15212dc08aa67
Kaivalya Dabhadkar <kdabhadkar@microsoft.com>
2022-12-06 11:23:58 +0530

    changed timestamp format

--- cmd/utils/utils.go
+++ cmd/utils/utils.go
@@ -6,8 +6,8 @@ import (
 )
 
 func ConvertTimeStampToUnixMicro(timestamp string) int64 {
-	layout := "2006-01-02T15:04:05.9-08:00"
-	t, err := time.Parse(layout, timestamp)
+	layout := "2006-01-02T15:04:05.9"
+	t, err := time.Parse(layout, timestamp[:len(timestamp)-7])
 	if err != nil {
 		fmt.Println(err)
 	}
